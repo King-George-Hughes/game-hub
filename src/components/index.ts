ac72f8103e8ee23fccdda66d6c66444884e20bc7
King-George-Hughes <hughesgeorgeme@gmail.com>
2023-09-20 23:32:51 +0000

    Adding platform icons

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,6 @@ import NavBar from "./NavBar";
 import ColorModeSwitch from "./ColorModeSwitch";
 import GameGrid from "./GameGrid";
 import GameCard from "./GameCard";
+import PlatformIconList from "./PlatformIconList";
 
-export { NavBar, ColorModeSwitch, GameGrid, GameCard };
+export { NavBar, ColorModeSwitch, GameGrid, GameCard, PlatformIconList };
