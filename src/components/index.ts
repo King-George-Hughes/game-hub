80d22361f9654105897a50ab819378d8d77ae6f7
King-George-Hughes <hughesgeorgeme@gmail.com>
2023-09-20 22:53:45 +0000

    building game card

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import NavBar from "./NavBar";
 import ColorModeSwitch from "./ColorModeSwitch";
 import GameGrid from "./GameGrid";
+import GameCard from "./GameCard";
 
-export { NavBar, ColorModeSwitch, GameGrid };
+export { NavBar, ColorModeSwitch, GameGrid, GameCard };
