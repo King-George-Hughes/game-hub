64e90634fe42c4d4025339324463b518394466b5
King-George-Hughes <hughesgeorgeme@gmail.com>
2023-10-14 08:35:27 +0000

    changes

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -14,30 +14,29 @@ const GameGrid = ({ gameQuery }: Props) => {
   const { data: games, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
 
-  return (
-    <>
-      {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        padding={10}
-        spacing={5}
-      >
-        {isLoading &&
-          skeletons.map((skeleton) => (
-            <GameCardContainer key={skeleton}>
-              <GameCardSkeleton />
-            </GameCardContainer>
-          ))}
+  if (error) return <Text>{error}</Text>;
 
-        {games.map((game) => {
-          return (
-            <GameCardContainer key={game.id}>
-              <GameCard game={game} />
-            </GameCardContainer>
-          );
-        })}
-      </SimpleGrid>
-    </>
+  return (
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      padding={10}
+      spacing={5}
+    >
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <GameCardContainer key={skeleton}>
+            <GameCardSkeleton />
+          </GameCardContainer>
+        ))}
+
+      {games.map((game) => {
+        return (
+          <GameCardContainer key={game.id}>
+            <GameCard game={game} />
+          </GameCardContainer>
+        );
+      })}
+    </SimpleGrid>
   );
 };
 
