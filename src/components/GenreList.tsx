b6d5bee2b39b777d5fbb6eeaa446f1f5927b4935
King-George-Hughes <hughesgeorgeme@gmail.com>
2023-10-14 07:50:49 +0000

    cleanup the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,6 +1,7 @@
 import {
   Button,
   HStack,
+  Heading,
   Image,
   List,
   ListItem,
@@ -22,31 +23,41 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   if (isLoading) return <Spinner />;
 
   return (
-    <List>
-      {genres.map((genre) => {
-        return (
-          <ListItem key={genre.id} paddingY={"5px"}>
-            <HStack>
-              <Image
-                src={getCroppedImage(genre.image_background)}
-                boxSize="50px"
-                borderRadius={8}
-              />
-              <Button
-                variant={"link"}
-                fontSize={"lg"}
-                key={genre.id}
-                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
-                onClick={() => onSelectGenre(genre)}
-                // onClick={() => console.log(genre)}
-              >
-                {genre.name}
-              </Button>
-            </HStack>
-          </ListItem>
-        );
-      })}
-    </List>
+    <>
+      <Heading marginBottom={2} fontSize={"2xl"}>
+        Genres
+      </Heading>
+      <List>
+        {genres.map((genre) => {
+          return (
+            <ListItem key={genre.id} paddingY={"5px"}>
+              <HStack>
+                <Image
+                  src={getCroppedImage(genre.image_background)}
+                  boxSize="50px"
+                  borderRadius={8}
+                  objectFit={"cover"}
+                />
+                <Button
+                  whiteSpace={"normal"}
+                  textAlign={"left"}
+                  variant={"link"}
+                  fontSize={"lg"}
+                  key={genre.id}
+                  fontWeight={
+                    genre.id === selectedGenre?.id ? "bold" : "normal"
+                  }
+                  onClick={() => onSelectGenre(genre)}
+                  // onClick={() => console.log(genre)}
+                >
+                  {genre.name}
+                </Button>
+              </HStack>
+            </ListItem>
+          );
+        })}
+      </List>
+    </>
   );
 };
 
