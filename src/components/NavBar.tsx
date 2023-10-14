b34c67e713933629cf25849b336fa6e5d7ff891a
King-George-Hughes <hughesgeorgeme@gmail.com>
2023-10-14 07:29:35 +0000

    implement search

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,12 +2,16 @@ import { HStack, Image } from "@chakra-ui/react";
 import { logo } from "../assets";
 import { ColorModeSwitch, SearchInput } from ".";
 
-const NavBar = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+const NavBar = ({ onSearch }: Props) => {
   return (
     // <HStack justifyContent={"space-between"} padding={"15px"}>
     <HStack padding={"15px"}>
       <Image src={logo} boxSize={"60px"} />
-      <SearchInput />
+      <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
   );
