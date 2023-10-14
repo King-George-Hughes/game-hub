1867198efbf993324241c6256dfb1e86f81b0157
King-George-Hughes <hughesgeorgeme@gmail.com>
2023-10-14 04:48:53 +0000

    filter games by platfrom

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,13 @@ import { Grid, GridItem, Show } from "@chakra-ui/react";
 import { GameGrid, GenreList, NavBar, PlatformSelector } from "./components";
 import { useState } from "react";
 import { Genre } from "./hooks/useGenres";
+import { Platform } from "./hooks/useGames";
 
 function App() {
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
+  const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(
+    null
+  );
 
   return (
     <>
@@ -30,8 +34,14 @@ function App() {
           </GridItem>
         </Show>
         <GridItem area={"main"}>
-          <PlatformSelector />
-          <GameGrid selectedGenre={selectedGenre} />
+          <PlatformSelector
+            selectedPlatform={selectedPlatform}
+            onSelectPlatform={(platform) => setSelectedPlatform(platform)}
+          />
+          <GameGrid
+            selectedGenre={selectedGenre}
+            selectedPlatform={selectedPlatform}
+          />
         </GridItem>
       </Grid>
     </>
