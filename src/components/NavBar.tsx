5e458ac29b07cb7baa98fd5e2c206b742b2e2d56
King-George-Hughes <hughesgeorgeme@gmail.com>
2023-10-14 06:57:56 +0000

    designing search

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image } from "@chakra-ui/react";
 import { logo } from "../assets";
-import { ColorModeSwitch } from ".";
+import { ColorModeSwitch, SearchInput } from ".";
 
 const NavBar = () => {
   return (
-    <HStack justifyContent={"space-between"} padding={"15px"}>
+    // <HStack justifyContent={"space-between"} padding={"15px"}>
+    <HStack padding={"15px"}>
       <Image src={logo} boxSize={"60px"} />
+      <SearchInput />
       <ColorModeSwitch />
     </HStack>
   );
