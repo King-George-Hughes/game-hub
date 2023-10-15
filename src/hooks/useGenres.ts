b7cb3339cbf8f632df98a4921cc5f0ef4cb1b230
King-George-Hughes <121091106+King-George-Hughes@users.noreply.github.com>
2023-10-15 18:00:18 +0000

    refactor hooks

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -14,7 +14,8 @@ const useGenres = () =>
     queryKey: ["genres"],
     queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, // 24hr
-    initialData: { count: genres.length, results: genres },
+    initialData: genres,
+    // initialData: { count: genres.length, results: genres, next: null },
   });
 
 export default useGenres;
